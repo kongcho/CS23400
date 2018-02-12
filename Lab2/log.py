87f8ddc68c43cd103aac27bff26f60ca28f539bf
CYK <kongc@uchicago.edu>
2018-02-12 16:26:16 -0600

    added both 2d and 1d data

--- Lab2/log.py
+++ Lab2/log.py
@@ -3,12 +3,20 @@ import ast
 import pprint
 import matplotlib.pyplot as plt
 import numpy as np
-from math import sqrt
+import math
 
-def get_speed(times, xs, ys):
+def get_speed_2d(times, xs, ys):
     vs = []
     for i in range(len(times) - 1):
-        d = sqrt((xs[i+1]-xs[i])**2 + (ys[i+1]-ys[i])**2)
+        d = math.sqrt((xs[i+1]-xs[i])**2 + (ys[i+1]-ys[i])**2)
+        t = times[i+1]-times[i]
+        vs.append(d/t)
+    return np.average(vs)
+
+def get_speed_1d(times, xs):
+    vs = []
+    for i in range(len(times) - 1):
+        d = abs(xs[i+1]-xs[i])
         t = times[i+1]-times[i]
         vs.append(d/t)
     return np.average(vs)
@@ -17,20 +25,37 @@ class Mac:
     def __init__(self, mac, logs):
         self.dic = {}
 #        d0s = []
+        xs = []
+        ys = []
         ds = []
         rsses = []
         logds = []
-        for log in logs:
-            for i in range(len(log.log["mac"])):
-                if mac == log.log["mac"][i]:
+        logxs = []
+        logys = []
+        for logg in logs:
+            for i in range(len(logg.log["mac"])):
+                if mac == logg.log["mac"][i]:
+                    rsses.append(logg.log["rss"][i])
  #                   d0s.append(log.d0)
-                    ds.append(log.ds[i])
-                    logds.append(math.log(log.ds[i]))
-                    rsses.append(log.log["rss"][i])
+                    ds.append(logg.ds[i])
+                    xs.append(logg.xs[i])
+                    ys.append(logg.ys[i])
+                    if i == 0:
+                        logds.append(0)
+                        logxs.append(0)
+                        logys.append(0)
+                    else:
+                        logds.append(math.log(logg.ds[i]))
+                        logxs.append(math.log(logg.xs[i]))
+                        logys.append(math.log(logg.ys[i]))
         self.mac = mac
 #        self.dic["d0s"] = d0s
-        self.dic["logds"] = logds
         self.dic["ds"] = ds
+        self.dic["xs"] = xs
+        self.dic["ys"] = ys
+        self.dic["logds"] = logds
+        self.dic["logxs"] = logxs
+        self.dic["logys"] = logys
         self.dic["rsses"] = rsses
 
 class Log:
@@ -51,11 +76,14 @@ class Log:
                 m.append(dictionary[ylabel])
             self.log[ylabel] = m
 
-        self.spd = get_speed(self.log["times"], self.log["loc_x"], self.log["loc_y"])
-
+        self.spd = get_speed_2d(self.log["times"], self.log["loc_x"], self.log["loc_y"])
         self.ds = [self.spd * t for t in self.log["times"]]
-
         self.d0 = self.ds[0]
+
+        self.spdx = get_speed_1d(self.log["times"], self.log["loc_x"])
+        self.spdy = get_speed_1d(self.log["times"], self.log["loc_y"])
+        self.xs = [self.spdx * t for t in self.log["times"]]
+        self.ys = [self.spdy * t for t in self.log["times"]]
 
     def showPlot(self):
         i=1
--- Lab2/main.py
+++ Lab2/main.py
@@ -2,6 +2,7 @@ from parse import parseFolder
 from log import Log, Mac
 
 macs = ["8c:85:90:16:0a:a4", "ac:9e:17:7d:31:e8", "d8:c4:6a:50:e3:b1", "f8:cf:c5:97:e0:9e"]
+# 8c:85:90:16:0a:a4 (location: x=6.8, y=6.8), ac:9e:17:7d:31:e8 (location: x=-0.87, y=9.45)
 
 def get_macs(folder):
     logs = parseFolder(folder)
@@ -10,4 +11,5 @@ def get_macs(folder):
         data.append(Mac(mac, logs))
     return data
 
-get_macs("./data")
+if __name__ == '__main__':
+    get_macs("./data")
