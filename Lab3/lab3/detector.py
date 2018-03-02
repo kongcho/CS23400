bc9a21c55e5f13cbf8834bd4d1a8a4be70f7b4fb
Natalia Kisanga <nkisanga@uchicago.edu>
2018-03-01 20:23:00 -0600

    algorithm for lab3

--- Lab3/lab3/detector.py
+++ Lab3/lab3/detector.py
@@ -9,32 +9,65 @@ from detector_wrapper import DetectorWrapper
 
 # ----------------------------------------------
 
+# set these values after testing?
+
+window_mid = 0      # midpoint of the window
+window_offset = 5   # level of sensitivity to midpoint of window
+time_offset = 1     # time offset for steering
+speed = 10          # speed level
+
+
+def drive(ret):
+
+    mid_x = ret[0]  # midpoint of the lane at the bottom border of a frame
+
+    # if midpoint of lane is to the right of the frame
+    if (mid_x > window_mid + window_offset):
+        front_wheels.turn_right()
+
+    # if midpoint of lane is to the left of the frame
+    if (mid_x < window_mid - window_offset):
+        front_wheels.turn_left()
+
+    # if midpoint of lane is relatively in the center
+    else:
+        front_wheels.turn_straight()
+
+    time.sleep(time_offset)
+
 
 # returns steering angle necessary to stay in lane
-def drive(ret):
-    dir = direction(ret)
-    if
+def turn_dir(ret):
 
-# returns the direction the lane is turning
-# calculate the curvature of a lane line, is to fit a 2nd degree polynomial to that line
-def direction(ret):
+    # calculate the curvature of a lane line, is to fit a 2nd degree polynomial to that line
 
-    # ret returns mid_x, left_fit, right_fit, ploty, left_fitx, right_fitx 
     # left_fit returns 3 values, 3 parameters of parabola of left lane
     # f(y) = Ay^2 + By + C
     # A gives us the curvature of the lane line, 
     # B gives us the heading or direction that the line is pointing
     # C gives us the position of the line based on how far away it is from the very left of the image
 
-    left_fit = ret[1]
-    right_ft = ret[2]
 
-    if ret[0] == 1:
-        return "straight"
-    else if ret[0] == 2:
-        return "left"
-    else 
-        return  "right"
+    left_fit = ret[1] # parameters for left lane
+    left_a = left_fit[0]
+    left_b = left_fit[1]
+    left_c = left_fit[2]
+
+
+    right_ft = ret[2] # parameters for right lane
+    right_a = right_fit[0]
+    right_b = right_fit[1]
+    right_c = right_fit[2]
+
+    # how to calculate curvature of lane??
+    curvature = (left_a + right_b)/2
+
+    # how is direction of wheel turn related to lane curvature?
+    # 0 < rel_angle < 30
+    direction = curvature/10
+
+    return direction
+
 
 
 def detect():
@@ -43,6 +76,11 @@ def detect():
 
         front_wheels = Front_Wheels()
         back_wheels = Back_Wheels()
+
+        #start driving
+        front_wheels.turn_straight()
+        back_wheels.speed = speed
+        back_wheels.forward()
 
         while True:
             success, ret = detector.detect()
@@ -55,8 +93,14 @@ def detect():
             # left_fit & right_fit are the fitted parameters for the parabola curve returned from numpy.polyfit
             # left_fitx and right_fitx save the x position(horizontal, increase from left to right)
 
-            angle = drive(ret)
-            turn_rel(angle)
+            # turns front wheels left, right, or straight
+            drive(ret)
+
+
+            # other option: turn wheels using angle calculation from curvature of lane
+
+            #angle = direction(ret)
+            #turn_rel(angle)
 
     except KeyboardInterrupt:
         print("KeboardInterrupt Captured")
