a6077275ccfc0e72de775bde9da789235895e553
CYK <kongc@uchicago.edu>
2018-03-02 14:24:00 -0600

    pre algos + some data

--- Lab3/lab3wk2/algo.py
+++ Lab3/lab3wk2/algo.py
@@ -5,53 +5,18 @@ import time
 import numpy as np
 
 detector = DetectorWrapper()
-
 front_wheels = Front_Wheels()
 back_wheels = Back_Wheels()
 
-# frame, mid_x , left_fit , right_fit , ploty , left_fitx , right_fitx
+# frame, mid_x, left_fit, right_fit, ploty, left_fitx, right_fitx
 
-def get_curvature(fit, y_eval):
+def get_curvature(fit, y):
     a, b, c = fit
-    curve_rad = ((1 + (2*a*y_eval + b)**2)**1.5) / np.absolute(2*a)
-
-    print(curve_rad)
+    curve_rad = ((1 + (2*a*y + b)**2)**1.5) / np.absolute(2*a)
     return curve_rad
-
-# turn left if detected lane center < middle etc
-def middle_lane():
-    print("START")
-    init = time.time()
-    drive = True
-    front_wheels.turn_straight()
-    back_wheels.speed = 50
-    front_wheels.speed = 50
-    while drive:
-        success, ret = detector.detect()
-        if time.time() - init > 5:
-            back_wheels.stop()
-            drive = False
-        elif success == 0:
-            back_wheels.stop()
-            print("ERROR")
-        else:
-            lane_center = (ret[6] - ret[5]) / 2 + ret[5]
-            dist_to_center = lane_center - mid_x
-            print(dist_to_center)
-            if dist_to_center < 0:
-                # turn left
-                back_wheels.speed = dist_to_center
-                front_wheels.turn_rel(dist_to_center)
-            else:
-                # turn right
-                back_wheels.speed = dist_to_center
-                front_wheels.turn_rel(dist_to_center)
-        print("END")
-    return
 
 # trying to see what kind of values u get
 def get_vals():
-#    back_wheels.stop()
     print("START")
     init = time.time()
     drive = True
@@ -61,30 +26,92 @@ def get_vals():
     while drive:
         if time.time() - init > 5:
             back_wheels.stop()
+            print("END of time")
             drive = False
         else:
             try:
                 back_wheels.forward()
                 success, ret = detector.detect()
-                print(time.time())
-                print("success: ")
-#                print(success.shape) 
-                print(success)
-                print("ret: ")
-#                print(ret.shape)
-                print(ret)
-#                print("left " + str(get_curvature(ret[2], ret[4])))
-#                print("right " + str(get_curvature(ret[3], ret[4])))
-                # detector.plot(ret[0])
+                print(ret[0].shape)
+                print(ret[4].shape)
+                print(ret[5].shape)
+                print(ret[6].shape)
+                print(ret[:4])
             except Exception as e:
-                print(e.message)
                 back_wheels.stop()
+                print(e)
                 print("ERROR")
         print("END")
     return
 
-if __name__ == "__main__":
-#    middle_lane()
+def log_vals(filename):
+    print("START")
+    init = time.time()
+    drive = True
+    is_first = True
+    all_arrs = []
+    front_wheels.turn_straight()
+    #TO CHANGE
+    back_wheels.speed = 50
+    front_wheels.speed = 50
+    while drive:
+        # TO CHANGE MAX TIME
+        if time.time() - init > 5:
+            back_wheels.stop()
+            print("FINISH TIME")
+            drive = False
+        else:
+            try:
+                # INSTRUCTIONS
+                back_wheels.forward()
 
-#    get_curvature()
-    get_vals()
+                # TURN LEFT
+#                back_wheels.forward()
+#                back_wheels.speed = 50
+#                front_wheels.turn_rel(-50)
+#                time.sleep(0.01)
+
+                # BETTER LEFT
+#                for i in range(70, 90, 1):
+#                    back_wheels.speed = (90 - i) * 4
+#                    front_wheels.turn(i)
+#                    time.sleep(3. / 20)
+
+                # OTHER
+                success, ret = detector.detect()
+                if success:
+                    frame, mid_x, left_fit, right_fit, ploty, left_fitx, right_fitx = ret
+                    curr_arr = [time.time(), mid_x, left_fit, right_fit, ploty, left_fitx, right_fitx]
+                    all_arrs.append(curr_arr)
+                    print(curr_arr[:4])
+
+                    if is_first:
+                        midpoint = mid_x
+                        is_first = False
+                        continue
+                    # CHANGE BASED ON MIDDLE
+
+                    # turn left
+                    # if mid_x < midpoint:
+                    #     for i in range(70, 80, 1):
+                    #         back_wheels.speed = (90 - i) * 4
+                    #         front_wheels.turn(-i)
+                    #         time.sleep(0.01)
+                    # turn right
+                    # else:
+                    #     for i in range(70, 80, 1):
+                    #         back_wheels.speed = (90 - i) * 4
+                    #         front_wheels.turn(i)
+                    #         time.sleep(0.01)
+                else:
+                    print("FAILED SUCCESS")
+            except Exception as e:
+                back_wheels.stop()
+                print("ERROR")
+                print(str(e))
+    all_arrs = np.array(all_arrs)
+    np.savetxt(filename, all_arrs, delimiter=',')
+    return
+
+if __name__ == "__main__":
+    log_vals("out.txt")
